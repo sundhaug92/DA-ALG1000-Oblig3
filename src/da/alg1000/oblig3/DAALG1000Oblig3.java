ae2ea471512503b45f75a1a2099589158be96413
Martin Sundhaug <martinsundhaug@gmail.com>
2013-04-09 12:39:16 +0200

    Base project

---
+++ src/da/alg1000/oblig3/DAALG1000Oblig3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package da.alg1000.oblig3;
+
+/**
+ *
+ * @author Martin
+ */
+public class DAALG1000Oblig3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
