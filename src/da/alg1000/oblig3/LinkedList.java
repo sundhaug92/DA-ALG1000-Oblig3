2d99143f5f87d59235212fcad0462440948c9b24
Martin Sundhaug <martinsundhaug@gmail.com>
2013-04-09 14:09:57 +0200

    Removed locking in LinkedList to conform with java.util.LinkedList

--- src/da/alg1000/oblig3/LinkedList.java
+++ src/da/alg1000/oblig3/LinkedList.java
@@ -8,7 +8,6 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.concurrent.locks.*;
 
 /**
  *
@@ -18,7 +17,6 @@ class LinkedList<T> extends java.util.LinkedList<T> {
 
     private Node<T> head;
     private int elements;
-    ReentrantLock lock = new ReentrantLock(true); //use a fair, reentrant lock
 
     public LinkedList() {
         head = null;
@@ -26,36 +24,21 @@ class LinkedList<T> extends java.util.LinkedList<T> {
     }
 
     public int getElements() {
-        lock.lock();
-        try {
-            return elements;
-        } finally {
-            lock.unlock();
-        }
+        return elements;
     }
 
     public Node<T> getHead() {
-        lock.lock();
-        try {
-            return head;
-        } finally {
-            lock.unlock();
-        }
+        return head;
     }
 
     public int Count(T element) {
         int count = 0;
-        lock.lock();
-        try {
-            Node<T> n = head;
-            for (int i = 0; i < elements; i++) {
-                if (n.getElement().equals(element)) {
-                    count++;
-                }
-                n = n.getNext();
+        Node<T> n = head;
+        for (int i = 0; i < elements; i++) {
+            if (n.getElement().equals(element)) {
+                count++;
             }
-        } finally {
-            lock.unlock();
+            n = n.getNext();
         }
         return count;
     }
@@ -63,164 +46,103 @@ class LinkedList<T> extends java.util.LinkedList<T> {
     @Override
     public String toString() {
         String s = new String();
-        lock.lock();
-        try {
-            Node<T> n = head;
-            for (int i = 0; i < elements; i++) {
-                s += "{" + n + "}";
-                if ((i % 5) == 4) {
-                    s += "\n";
-                }
-                n = n.getNext();
+        Node<T> n = head;
+        for (int i = 0; i < elements; i++) {
+            s += "{" + n + "}";
+            if ((i % 5) == 4) {
+                s += "\n";
             }
-        } finally {
-            lock.unlock();
+            n = n.getNext();
         }
         return s;
     }
 
     public void reset() {
-        lock.lock();
-        try {
-            elements = 0;
-            head = null;
-        } finally {
-            lock.unlock();
-        }
+        elements = 0;
+        head = null;
     }
 
     private Node<T> nodeWithIndex(int index) {
-        lock.lock();
-        try {
-            if (index < 0) {
-                return null;
-            }
-            Node<T> n = head;
-            for (int i = 0; i < index; i++) {
-                n = n.getNext();
-            }
-            return n;
-        } finally {
-            lock.unlock();
+        if (index < 0) {
+            return null;
         }
+        Node<T> n = head;
+        for (int i = 0; i < index; i++) {
+            n = n.getNext();
+        }
+        return n;
     }
 
     private Node<T> nodeWithElement(T element) {
-        lock.lock();
-        try {
-            if (head != null) {
-                if (head.getElement().equals(element)) {
-                    return head;
-                }
-                Node<T> n = head;
-                for (int i = 0; i < elements; i++) {
-                    if (n.getElement().equals(element)) {
-                        return n;
-                    }
-                    n = n.getNext();
-                }
+        if (head != null) {
+            if (head.getElement().equals(element)) {
+                return head;
             }
-            return null;
-
-        } finally {
-            lock.unlock();
+            Node<T> n = head;
+            for (int i = 0; i < elements; i++) {
+                if (n.getElement().equals(element)) {
+                    return n;
+                }
+                n = n.getNext();
+            }
         }
+        return null;
     }
 
     void addItem(T element, int index) {
-        lock.lock();
-        try {
-            Node<T> n = nodeWithIndex(index - 1);
-            if (n != null) {
-                n.setNext(new Node(element, n.getNext()));
-            } else if ((index <= 0) && (head != null)) {
-                head = new Node(element, head);
-            } else if ((index <= 0) && (head == null)) {
-                head = new Node(element, null);
-            }
-            elements++;
-        } finally {
-            lock.unlock();
+        Node<T> n = nodeWithIndex(index - 1);
+        if (n != null) {
+            n.setNext(new Node(element, n.getNext()));
+        } else if ((index <= 0) && (head != null)) {
+            head = new Node(element, head);
+        } else if ((index <= 0) && (head == null)) {
+            head = new Node(element, null);
         }
+        elements++;
     }
 
     void addItem(T element) {
-        try {
-            addItem(element, elements);
-        } finally {
-            lock.unlock();
-        }
+        addItem(element, elements);
     }
 
     void removeItem(int index) {
-        lock.lock();
-        try {
-            if (head != null) {
-                if (index == 0) {
-                    head = head.getNext();
-                } else {
-                    Node<T> n = nodeWithIndex(index - 1);
-                    n.setNext(n.getNext().getNext());
-                }
-                elements--;
+        if (head != null) {
+            if (index == 0) {
+                head = head.getNext();
+            } else {
+                Node<T> n = nodeWithIndex(index - 1);
+                n.setNext(n.getNext().getNext());
             }
-        } finally {
-            lock.unlock();
+            elements--;
         }
     }
 
     int indexOfFirstMatch(T element) {
-        lock.lock();
-        try {
-            Node<T> target = nodeWithElement(element), n = head;
-            for (int i = 0; i < elements; i++) {
-                if (n.equals(target)) {
-                    return i;
-                }
-                n = n.getNext();
+        Node<T> target = nodeWithElement(element), n = head;
+        for (int i = 0; i < elements; i++) {
+            if (n.equals(target)) {
+                return i;
             }
-            return -1;
-        } finally {
-            lock.unlock();
+            n = n.getNext();
         }
+        return -1;
     }
 
     void removeItemFirstMatch(T matchElement) {
-        lock.lock();
-        try {
-            removeItem(indexOfFirstMatch(matchElement));
-        } finally {
-            lock.unlock();
-        }
+        removeItem(indexOfFirstMatch(matchElement));
     }
 
     void addItemAfterFirstMatch(T element, T matchElement) {
-        lock.lock();
-        try {
-            addItem(element, indexOfFirstMatch(matchElement) + 1);
-        } finally {
-            lock.unlock();
-        }
+        addItem(element, indexOfFirstMatch(matchElement) + 1);
     }
 
     void addItemBeforeFirstMatch(T element, T matchElement) {
-        lock.lock();
-        try {
-            addItem(element, indexOfFirstMatch(matchElement) - 1);
-
-        } finally {
-            lock.unlock();
-        }
+        addItem(element, indexOfFirstMatch(matchElement) - 1);
     }
 
     void removeLastItem() {
-        lock.lock();
-        try {
-            removeItem(elements - 1);
+        removeItem(elements - 1);
 
-        } finally {
-            lock.unlock();
-        }
     }
 
     @Override
