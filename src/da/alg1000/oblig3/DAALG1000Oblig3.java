ea952d00db40b0e916d2863806b3a7c8fb0d2a96
Martin Sundhaug <martinsundhaug@gmail.com>
2013-04-09 13:57:14 +0200

    Added application logic

--- src/da/alg1000/oblig3/DAALG1000Oblig3.java
+++ src/da/alg1000/oblig3/DAALG1000Oblig3.java
@@ -4,6 +4,8 @@
  */
 package da.alg1000.oblig3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Martin
@@ -14,6 +16,14 @@ public class DAALG1000Oblig3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner inp = new Scanner(System.in);
+        hashTable<String> ht = new hashTable(15);
+
+        for (Integer i = 0;; i++) {
+            String str = inp.nextLine();
+            System.out.println(ht.hash(str) + "," + ht.getBucketIdFor(str));
+            ht.add(i.toString(), str);
+            System.out.println(ht.get(i.toString()));
+        }
     }
 }
