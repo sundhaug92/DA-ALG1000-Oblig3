9ff9a5daba215cb7e47ee6042f4191e1e8af0cbb
Martin Sundhaug <martinsundhaug@gmail.com>
2013-04-09 13:07:20 +0200

    Added hashTable

---
+++ src/da/alg1000/oblig3/hashTable.java
@@ -0,0 +1,109 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package da.alg1000.oblig3;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Martin
+ */
+public class hashTable<T> {
+
+    ArrayList<LinkedList<AbstractMap.SimpleEntry<String,T>>> buckets;
+
+    public hashTable(int numberOfBuckets) {
+        buckets = new ArrayList<>();
+        for (int i = 0; i < numberOfBuckets; i++) {
+            buckets.add(new LinkedList<AbstractMap.SimpleEntry<String,T>>());
+        }
+    }
+
+    T get(String name) {
+        return getBucketFor(name).get(getIndexFor(name)).getValue();
+    }
+    void set(String name, T value){
+        getBucketFor(name).get(getIndexFor(name)).setValue(value);
+    }
+
+    LinkedList<AbstractMap.SimpleEntry<String,T>> getBucketFor(String name) {
+        return buckets.get(getBucketIdFor(name));
+    }
+
+    int getIndexFor(String name) {
+        LinkedList<AbstractMap.SimpleEntry<String,T>> bucket=getBucketFor(name);
+        int i=0;
+        for(Object o:bucket.toArray()){
+            AbstractMap.SimpleEntry<String,T> entry=(AbstractMap.SimpleEntry<String,T>)o;
+            if(entry.getKey()==name)return i;
+            i++;
+        }
+        return 0;
+    }
+
+    String hash(String name) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+        String s = "";
+        byte[] buf = md.digest(name.getBytes());
+        for (byte b : buf) {
+            s += toHex(b);
+        }
+        return s;
+    }
+
+    private static String toHex(byte b) {
+        String s = "";
+        int n = (b > 0) ? b : 256 + b;
+        s += halfToHex(n >> 4);
+        s += halfToHex(n & 15);
+        return s;
+    }
+
+    private static char halfToHex(int i) {
+        if (i < 10) {
+            return (char) ('0' + i);
+        } else {
+            return (char) ('A' + (i - 10));
+        }
+    }
+
+    int getBucketIdFor(String name) {
+        int i = 0;
+        String h = "";
+        try {
+            h = hash(name).substring(0, 7);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(hashTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        i = Integer.parseInt(h.toLowerCase(), 16);
+        return i % buckets.size();
+    }
+
+    int size() {
+        int r = 0;
+        for (LinkedList<AbstractMap.SimpleEntry<String,T>> bucket : buckets) {
+            r += bucket.size();
+        }
+        return r;
+    }
+
+    int calculateLoadFactor() {
+        return size() / buckets.size();
+    }
+
+    void add(String name, T value) {
+        getBucketFor(name).add(new AbstractMap.SimpleEntry<>(name,value));
+    }
+    void remove(String name){
+        getBucketFor(name).remove(name);
+    }
+}
