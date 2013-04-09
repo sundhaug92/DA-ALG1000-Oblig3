831ca5f3dea35ace63e51f8f52cb5d0a57cab684
Martin Sundhaug <martinsundhaug@gmail.com>
2013-04-09 14:48:37 +0200

    Set key manually in test program

--- src/da/alg1000/oblig3/DAALG1000Oblig3.java
+++ src/da/alg1000/oblig3/DAALG1000Oblig3.java
@@ -19,11 +19,10 @@ public class DAALG1000Oblig3 {
         Scanner inp = new Scanner(System.in);
         hashTable<String> ht = new hashTable(15);
 
-        for (Integer i = 0;; i++) {
-            String str = inp.nextLine();
-            System.out.println(ht.hash(str) + "," + ht.getBucketIdFor(str));
-            ht.add(i.toString(), str);
-            System.out.println(ht.get(i.toString()));
+        for (;;) {
+            String key = inp.nextLine();
+            String value = inp.nextLine();
+            ht.add(key, value);
         }
     }
 }
