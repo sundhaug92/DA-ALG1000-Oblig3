6fc822d6e6eea8c3792c2b5636f1fcd5d7166a9b
Martin Sundhaug <martinsundhaug@gmail.com>
2013-04-24 00:26:23 +0200

    HTTP based exitManager

--- src/da/alg1000/oblig3/exitManager.java
+++ src/da/alg1000/oblig3/exitManager.java
@@ -3,60 +3,88 @@
  * and open the template in the editor.
  */
 package da.alg1000.oblig3;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 import javax.sound.midi.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.net.URL;
 
 /**
  *
  * @author Martin
  */
 public final class exitManager {
-    Synthesizer synth;
-    void tone(int note, double length) throws MidiUnavailableException, InterruptedException{
-            MidiChannel[] channels=synth.getChannels();
-        int fullNote=1250*4/3, baseNote=60, velocity=100;
-            channels[5].noteOn(baseNote+note, velocity);
-            Thread.sleep((int)(length*fullNote));
-            channels[5].noteOff(baseNote+note);
+
+    void printSenter(String str) {
+        for (int i = (40 - str.length()) / 2; i > 0; i--) {
+            System.out.print(" ");
+        }
+        System.out.println(str);
     }
-    exitManager(int instrument) {
+
+    exitManager(int instrument) throws MalformedURLException, IOException, MidiUnavailableException, InvalidMidiDataException {
         try {
-            synth=MidiSystem.getSynthesizer();
-            synth.open();
-            Instrument[] instr=synth.getDefaultSoundbank().getInstruments();
-            synth.getChannels()[5].programChange(instrument);
-            if(synth.getChannels()[5].getProgram()==instrument){
-            tone(5,0.25f);
-            tone(2,0.125f);
-            tone(3,0.125f);
-            
-            tone(4,0.25f);
-            tone(3,0.125f);
-            tone(2,0.125f);
-            
-            tone(1,0.25f);
-            tone(1,0.125f);
-            tone(3,0.125f);
-            
-            tone(5,0.25f);
-            tone(4,0.125f);
-            tone(3,0.125f);
-            
-            tone(2,0.25f);
-            tone(2,0.125f);
-            tone(3,0.125f);
-            
-            tone(4,0.25f);
-            tone(5,0.25f);
-            
-            tone(3,0.25f);
-            tone(1,0.25f);
-            tone(1,0.25f);
-            synth.close();
-    }
+            URL url = new URL("http://www.tsr.org/StarWars/multimedia/midi/thronenend.mid");
+
+            System.out.println("Please wait...");
+            final Sequence sequence = MidiSystem.getSequence(url);
+            final Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequencer.setSequence(sequence);
+            sequencer.start();
+            while (!sequencer.isRunning()) {
+                Thread.sleep(1);
+            }
+            for (int i = 18; i > 0; i--) {
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+            for (int i = 25; i > 0; i--) {
+                System.out.println();
+            }
+            printSenter("Obligatorisk innlevering 3 avsluttes");
+            Thread.sleep(3000);
+            printSenter("Semesteret likedan");
+            Thread.sleep(3000);
+            printSenter("Ny kunnskap tilegnet");
+            Thread.sleep(3000);
+            printSenter("Kompleksitet beregnet");
+            Thread.sleep(3000);
+            printSenter("Grafer tegnet");
+            Thread.sleep(3000);
+            printSenter("Linjer kodet");
+            Thread.sleep(3000);
+            printSenter("Java i hodet");
+            Thread.sleep(3000);
+            printSenter("End->shift+opp lett");
+            Thread.sleep(3000);
+            printSenter("Linje med .NET");
+            Thread.sleep(3000);
+            printSenter("Merk den og slett");
+            Thread.sleep(3000);
+            printSenter("Gjennom kildekoden sett");
+            Thread.sleep(3000);
+            printSenter("Derhvor teksten er tett");
+            Thread.sleep(3000);
+            printSenter("Programlus blir mett");
+            Thread.sleep(3000);
+            printSenter("Algoritmen dertil");
+            Thread.sleep(3000);
+            printSenter("Minst mulig snill");
+            Thread.sleep(3000);
+            printSenter("Exception-handleren vondt vil");
+            Thread.sleep(3000);
+            printSenter("Mottageren av brukerens SIGKILL");
+            Thread.sleep(3000);
+            printSenter("");
+            printSenter("");
+            printSenter("");
+            printSenter("\"Throne Room & The End Titles\" - John Williams (midi by Dolimah Deen)");
+            printSenter("Software by Martin Sundhaug");
+
+
         } catch (Exception ex) {
-            Logger.getLogger(exitManager.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex);
         }
     }
 }
